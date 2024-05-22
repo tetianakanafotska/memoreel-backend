a372d089e69455ab7668a849865825a3694d993d
Laura Sinclair <l.sinclair@tuta.io>
2024-05-22 15:27:56 +0200

    Setup board routes

--- models/Board.model.js
+++ models/Board.model.js
@@ -1,7 +1,7 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
 const boardSchema = new Schema({
-  timestamps: true,
-});
+  "boardContent": {}
+}, { timestamps: true });
 
-const Board = model(Board, boardSchema);
+module.exports = model('Board', boardSchema);
---
+++ routes/boards.routes.js
@@ -0,0 +1,70 @@
+const router = require('express').Router();
+const Board = require('../models/Board.model.js');
+
+
+// get all boards
+router.get('/', (req, res, next) => {
+	Board.find()
+		.then((allBoards) => {
+			res.status(200).json(allBoards);
+		})
+		.catch((error) => {
+			res.status(500).json({ error: 'Failed to fetch all the boards' });
+		});
+});
+
+
+// post a new board
+router.post('/', (req, res, next) => {
+	Board.create(req.body)
+		.then((createdBoard) => {
+			res.status(201).json(createdBoard);
+		})
+		.catch((error) => {
+			res.status(500).json({ error: 'Failed to create a new board' });
+		});
+});
+
+
+// get a single board
+router.get('/:boardId', (req, res, next) => {
+	const {boardId} = req.params
+
+	Board.findById(boardId)
+		.then((board) => {
+			res.status(200).json(board);
+		})
+		.catch((error) => {
+			res.status(500).json({ error: 'Failed to fetch this board' });
+		});
+});
+
+
+// edit a single board
+router.put('/:boardId', (req, res, next) => {
+	const {boardId} = req.params
+
+	Board.findByIdAndUpdate(boardId, req.body, updatedBoard)
+		.then((updatedBoard) => {
+			res.status(204).json(updatedBoard);
+		})
+		.catch((error) => {
+			res.status(500).json({ error: 'Failed to update this board' });
+		});
+});
+
+
+// delete a single board
+router.delete('/:boardId', (req, res, next) => {
+  const {boardId} = req.params
+
+	Board.findByIdAndDelete(boardId)
+		.then((result) => {
+      res.status(204).send();
+		})
+		.catch((error) => {
+			res.status(500).json({ error: 'Failed to delete this board' });
+		});
+});
+
+module.exports = router;
