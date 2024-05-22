7e8e479c37f33cc03511797a89c573f90409937e
ecemonkol <ecemonkol@gmail.com>
2024-05-22 15:28:36 +0200

    add user routes

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ require("./config")(app);
 // 👇 Start handling routes here
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
+const userRoutes = require("./routes/user.routes");
+app.use("/user", userRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
---
+++ routes/user.routes.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+const User = require("../models/User.model");
+
+router.get("/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (user) {
+        res.json(user);
+      } else {
+        res.status(404).json({ message: "User not found" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ message: error.message });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (user) {
+        if (req.body.name != null) {
+          user.name = req.body.name;
+        }
+        if (req.body.email != null) {
+          user.email = req.body.email;
+        }
+        if (req.body.password != null) {
+          user.password = req.body.password;
+        }
+
+        user
+          .save()
+          .then((updatedUser) => {
+            res.json(updatedUser);
+          })
+          .catch((error) => {
+            res.status(400).json({ message: error.message });
+          });
+      } else {
+        res.status(404).json({ message: "User not found" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ message: error.message });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  User.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.json({ message: "User deleted" });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+module.exports = router;
